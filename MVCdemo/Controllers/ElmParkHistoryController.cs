fb692dc7b7194f56257dfb1f8bc930f399d313bf
Alan Barrett <Alan Barrett@DESKTOP-9TGDPG9>
2016-05-16 18:31:07 +0100

    Add project files.

---
+++ MVCdemo/Controllers/ElmParkHistoryController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCdemo.Controllers
+{
+    public class ElmParkHistoryController : Controller
+    {
+        // GET: ElmParkHistory
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index2()
+        {
+            return View();
+        }
+
+        public ActionResult Index3()
+        {
+            return View();
+        }
+    }
+}
